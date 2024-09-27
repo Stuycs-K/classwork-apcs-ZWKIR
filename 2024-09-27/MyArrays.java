bbc8e8f354b15f6516deafcbacaf4e8f94c9dc59
Amy Shrestha <am226ys@gmail.com>
2024-09-27 11:47:19 -0400

    added a few tests

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -11,9 +11,18 @@ public class MyArrays {
   }
 
   public static void main(String[] args) {
+    // test arrayToString
     int[] arr = new int[] {1, 2, 3};
     System.out.println("Expected [1, 2, 3], recieved " + arrayToString(arr));
     int[] arr2 = new int[] {};
     System.out.println("Expected [], recieved " + arrayToString(arr2));
+
+    // test returnCopy
+    int[] arr = new int[] {2, 5, 1, 35};
+    int[] arr2 = new int[] {24, 2, 1, 4};
+    System.out.println("Expected true " + "recieved " + (arr == arr2));
+
+    // test concatArray
+
   }
 }
