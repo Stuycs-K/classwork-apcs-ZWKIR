18f91c39f54c3b7db54953542eaca12fc9f50b33
Amy Shrestha <am226ys@gmail.com>
2024-10-15 21:15:43 -0400

    added triangle teste

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -21,17 +21,17 @@ public class Demo{
     System.out.println(p1);
     System.out.println(p5);
 
-    System.out.println("------------testing Demo.distance()------------");
+    System.out.println("\n------------testing Demo.distance()------------");
     System.out.println( distance(p1,p2));
     System.out.println( distance(p2,p3));
     System.out.println( distance(p1,p3));
 
-    System.out.println("------------testing Point.distance()------------");
+    System.out.println("\n------------testing Point.distance()------------");
     System.out.println( Point.distance(p1,p2));
     System.out.println( Point.distance(p2,p3));
     System.out.println( Point.distance(p1,p3));
 
-    System.out.println("------------testing nonstatic distanceTo()------------");
+    System.out.println("\n------------testing nonstatic distanceTo()------------");
     System.out.println( p1.distanceTo(p2));
     System.out.println( p2.distanceTo(p3));
     System.out.println( p1.distanceTo(p3));
@@ -39,5 +39,10 @@ public class Demo{
     Point t1 = new Point(0, 0);
     Point t2 = new Point(1, 0);
     Point t3 = new Point(0.5, Math.sqrt(3)/2);
+
+    System.out.println("\n------------testing triangle------------");
+    System.out.println(t1.distanceTo(t2));
+    System.out.println(t1.distanceTo(t3));
+    System.out.println(t2.distanceTo(t3));
   }
 }
