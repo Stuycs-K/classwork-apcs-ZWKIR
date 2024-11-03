49368dd0aed157fb83017cb350598af498a64a66
Amy Shrestha <am226ys@gmail.com>
2024-11-03 17:50:25 -0500

    finished first part of isTriangle

--- 10-31-Advent/Main.java
+++ 10-31-Advent/Main.java
@@ -25,6 +25,7 @@ public class Main {
       System.out.println(sum);
       */
 
+      /*
       Scanner sc1 = new Scanner("This is a bunch of words");
       while (sc1.hasNext()) {
         System.out.println(sc1.next());
@@ -36,5 +37,9 @@ public class Main {
         sum += sc2.nextDouble();
       }
       System.out.println(sum);
+      */
+
+      // Testing TriangleTester methods
+      System.out.println(TriangleTester.isTriangle("5 10 25"));
     }
 }
--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
 public class TriangleTester {
   public static int countTrianglesA(String filename) {
-    return 0;
+    int count = 0;
+    return count;
+  }
+
+  public static boolean isTriangle(String lengths) {
+    Scanner scan = new Scanner(lengths);
+    int[] ary = new int[3];
+    while (scan.hasNext()) {
+      for (int i = 0; i < ary.length; i++) {
+        ary[i] = Integer.parseInt(scan.next());
+      }
+    }
+    System.out.println(toString(ary));
+    return false;
   }
 
   public static int countTrianglesB(String filename) {
     return 0;
+  }
+
+  public static String toString(int[] nums) {
+    String returnStr = "[";
+    for (int i = 0; i < nums.length; i++) {
+      returnStr += nums[i];
+      if (i < nums.length - 1) {
+        returnStr += ", ";
+      }
+    }
+    return returnStr + "]";
   }
 }
