f7ac9d9d3008ff8711b36d424a5178068a1cb817
Amy Shrestha <am226ys@gmail.com>
2024-11-03 17:27:58 -0500

    created TriangleTester

---
+++ 10-31-Advent/TriangleTester.java
@@ -0,0 +1,9 @@
+public class TriangleTester {
+  public static int countTrianglesA(String filename) {
+    return 0;
+  }
+
+  public static int countTrianglesB(String filename) {
+    return 0;
+  }
+}
