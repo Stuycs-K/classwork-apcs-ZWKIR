6029310166ea2c30e22975e9607e4e914fcda914
Amy Shrestha <am226ys@gmail.com>
2024-11-05 20:01:03 -0500

    changed to ArrayList

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TriangleTester {
@@ -30,33 +31,19 @@ public class TriangleTester {
     return (ary[0] + ary[1] > ary[2] && ary[1] + ary[2] > ary[0] && ary[0] + ary[2] > ary[1]);
   }
 
-  public static int numLines(String file) {
-    int count = 0;
-    try {
-      Scanner scanned = new Scanner(new File(file));
-      while (scanned.hasNextLine()) {
-        scanned.nextLine();
-        count++;
-      }
-      scanned.close();
-    } catch (FileNotFoundException e)  {
-      System.err.println("File not found");
-    }
-    return count;
-  }
-
   public static int countTrianglesB(String filename) {
     int numTri = 0;
-    int numRow = numLines(filename);
+    ArrayList<int[]> ary = new ArrayList<>();
     try {
       Scanner scan = new Scanner(new File(filename));
-      int[][] ary = new int[numRow][3];
-      for (int i = 0; i < numRow; i++) {
-        for (int j = 0; j < 3; i++) {
+      while (scan.hasNextInt()) {
+        int[] ary2 = new int[3];
+        for (int i = 0; i < 3; i++) {
           if (scan.hasNextInt()) {
-            ary[i][j] = scan.nextInt();
+            ary2[i] = scan.nextInt();
           }
         }
+        ary.add(ary2);
       }
       scan.close();
     } catch (FileNotFoundException e)  {
@@ -65,11 +52,16 @@ public class TriangleTester {
     return numTri;
   }
 
+  // helper function for countTrianglesB
+  public static boolean isTriangle(int[] lengths) {
+    return (lengths[0] + lengths[1] > lengths[2] && lengths[1] + lengths[2] > lengths[0] && lengths[0] + lengths[2] > lengths[1]);
+  }
+
   public static void main(String[] args) {
     // Tests
     System.out.println(isTriangle("5 10 25")); // false
     System.out.println(isTriangle("3 4 5")); // true
     System.out.println(countTrianglesA("inputTri.txt"));
-    System.out.println(numLines("inputTri.txt"));
+    System.out.println(countTrianglesB("inputTri.txt")); // 6 for first dataset
   }
 }
