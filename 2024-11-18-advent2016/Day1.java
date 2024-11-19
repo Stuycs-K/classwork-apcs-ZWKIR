e31e54cb108c8d7e5713122fa154081c0e411d08
Amy Shrestha <am226ys@gmail.com>
2024-11-18 20:03:24 -0500

    fixed up shortestDist code

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -1,62 +1,44 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Day1 {
-  public static int shortestDist(String filename) {
+  public static String[] shortestDist(String filename) {
     int yCount = 0;
     int xCount = 0;
     int deg = 0;
     try {
       Scanner file = new Scanner(new File(filename));
-      while (file.hasNext(", ")) {
-        if(file.next(", ").substring(0, 2) == "R") {
-          deg += 90;
-          if (deg >= 360) {
-            deg = deg - 360;
-          }
-          if (deg == 90) {
-            xCount += (file.nextInt());
-          }
-          else if (deg == 270) {
-            xCount -= (file.nextInt());
-          }
-          else if (deg == 180) {
-            yCount -= (file.nextInt());
-          }
-          else {
-            yCount += (file.nextInt());
+      if (file.hasNextLine()) {
+        String[] ary = file.nextLine().split(", ");
+        for (int i = 0; i < ary.length; i++) {
+          if (ary[i].substring(0,2).equals("R")) {
+            deg += 90;
+            xCount += Integer.parseInt(ary[i].substring(1));
           }
         }
-        else {
-          deg -= 90;
-          if (deg >= 360) {
-            deg = deg - 360;
-          }
-          if (deg == 90) {
-            xCount += (file.nextInt());
-          }
-          else if (deg == 270) {
-            xCount -= (file.nextInt());
-          }
-          else if (deg == 180) {
-            yCount -= (file.nextInt());
-          }
-          else {
-            yCount += (file.nextInt());
-          }
-        }
+        return ary;
       }
       file.close();
     } catch (FileNotFoundException e)  {
       System.err.println("File not found");
     }
-    return Math.abs(xCount) + Math.abs(yCount);
+    return new String[2];
+  }
+
+  public static String toString(String[] nums) {
+    String returnStr = "[";
+    for (int i = 0; i < nums.length; i++) {
+      returnStr += nums[i];
+      if (i < nums.length - 1) {
+        returnStr += ", ";
+      }
+    }
+    return returnStr + "]";
   }
 
   public static void main(String[] args) {
     // Tests
-    System.out.println(shortestDist("day1.txt"));
+    System.out.println(toString(shortestDist("day1.txt")));
   }
 }
