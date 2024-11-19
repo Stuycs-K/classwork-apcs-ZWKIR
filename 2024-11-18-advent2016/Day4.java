ed8e2abe50a57490ede3940e7e0df55a3e8c9efb
Amy Shrestha <am226ys@gmail.com>
2024-11-18 21:30:16 -0500

    finished realRoom() and test

--- 2024-11-18-advent2016/Day4.java
+++ 2024-11-18-advent2016/Day4.java
@@ -1,10 +1,22 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Day4 {
   public static boolean realRoom(String room) {
-    return false;
+    int lastDashI = room.lastIndexOf('-');
+    int checkSumI = room.indexOf('[');
+    String roomName = room.substring(0, lastDashI);
+    String sectorID = room.substring(lastDashI+1, checkSumI);
+    String sum = room.substring(checkSumI+1, room.length()-1);
+    roomName = roomName.replace("-", "");
+    String checkSum = myCheckSum(roomName);
+    return checkSum.equals(sum);
+  }
+
+  public static String myCheckSum(String check) {
+    return "";
   }
 
   public static int rooms(String filename) {
@@ -13,9 +25,11 @@ public class Day4 {
       Scanner file = new Scanner(new File(filename));
       while (file.hasNextLine()) {
         String str = file.nextLine();
+        /*
         if (realRoom(str)) {
           roomCount += 1;
         }
+        */
       }
       file.close();
     } catch (FileNotFoundException e)  {
@@ -24,7 +38,19 @@ public class Day4 {
     return roomCount;
   }
 
+  public static String toString(String[] nums) {
+    String returnStr = "[";
+    for (int i = 0; i < nums.length; i++) {
+      returnStr += nums[i];
+      if (i < nums.length - 1) {
+        returnStr += ", ";
+      }
+    }
+    return returnStr + "]";
+  }
+
   public static void main(String[] args) {
-    System.out.println(rooms("day4.txt"));
+    System.out.println(realRoom("aaaaa-bbb-z-y-x-123[abxyz]"));
+    //System.out.println(rooms("day4.txt"));
   }
 }
