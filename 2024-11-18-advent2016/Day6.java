5049d6ecd282cd7fbe5ca15dfd10f7612d95f252
Amy Shrestha <am226ys@gmail.com>
2024-11-20 11:46:40 -0500

    created loop for freq()

--- 2024-11-18-advent2016/Day6.java
+++ 2024-11-18-advent2016/Day6.java
@@ -22,13 +22,31 @@ public class Day6 {
     for (int i = 0; i < 26; i++) {
       alphabet[i] = (char) ('a' + i);
     }
-
-    return count;
     // create arraylist of each line, separated by letter,
     // check index of each character and count the occurences
     // in each index in another array, return a message
     // with the character that is most frequent at each
     // index (if multiple w highest freq, use any most frequent)
+    ArrayList<char[]> ary = new ArrayList<>();
+    try {
+        Scanner scan = new Scanner(new File(filename));
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+            Scanner scanned = new Scanner(line);
+            char[] ary2 = new char[6];
+
+            for (int i = 0; i < 6; i++) {
+                ary2[i] = line.charAt(i);
+            }
+            ary.add(ary2);
+            scanned.close();
+            System.out.println(toString(ary));
+        }
+        scan.close();
+    } catch (FileNotFoundException e) {
+        System.err.println("File not found");
+    }
+    return count;
   }
 
   public static void main(String[] args) {
