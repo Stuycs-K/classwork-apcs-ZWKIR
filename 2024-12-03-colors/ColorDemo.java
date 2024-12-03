8e82e75d5d13fd2d9063060e5fa6e15311f9e37e
Amy Shrestha <am226ys@gmail.com>
2024-12-03 11:55:36 -0500

    started on a color practice

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,20 @@
+public class ColorDemo {
+  public static int BLACK = 30;
+  public static int RED = 31;
+
+  public static void color(int foreground,int background){
+    System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
+  }
+
+  public static void color(int foreground, int background, int modifier){
+    System.out.print( "\u001b[" + foreground + ";" + (10+background) + ";" + modifier + "m");
+  }
+
+  public static void go(int r,int c){
+    System.out.print("\u001b[" + r + ";" + c + "f");
+  }
+
+  public static void drawFrog() {
+    System.out.println(color(35, 41, "m") + "This is a frog.");
+  }
+}
