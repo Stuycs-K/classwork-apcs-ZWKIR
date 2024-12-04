fd49927087e26d09ae2897fa5c28507a13654be5
Amy Shrestha <am226ys@gmail.com>
2024-12-04 11:21:12 -0500

    started on border

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,5 +1,14 @@
 public class Driver {
+  public static void border(int text, int back) {
+    for (int i = 1; i <= 2; i++) {
+      for (int j = 1; j <= 30; j++) {
+        Text.color(text, back);
+        System.out.println(" ");
+      }
+      System.out.println("");
+    }
+  }
   public static void main(String[] args) {
-    System.out.println("hi");
+    border(30, 42);
   }
 }
