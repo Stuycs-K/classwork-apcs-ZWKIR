7c42f77b6cc2bb142445bfa5d8a9dfe29c36b412
Amy Shrestha <am226ys@gmail.com>
2024-12-04 19:20:37 -0500

    finished right border

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,28 +1,32 @@
 public class Driver {
   public static void border(int text, int back, int height, int length) {
+    int count = 0;
     for (int i = 1; i <= height; i++) {
-      int count = 0;
+      Text.color(text, 47);
       if (i == 1 || i == height) {
-        Text.color(text, back);
-        while (count < length) {
-          System.out.print(" ");
-          count++;
-        }
-        count = 0;
+        count = repeat(count, length);
       }
-      /*
       else {
-        System.out.println(" ");
+        System.out.println("");
+        System.out.print(" ");
         System.out.print(Text.RESET);
-        System.out.println(" ");
-        Text.color(text, back);
-        System.out.println(" ");
+        count = repeat(count, length - 2);
+        Text.color(text, 44);
+        System.out.print(" ");
       }
-      */
       System.out.print(Text.RESET);
-      System.out.println("");
     }
   }
+
+  public static int repeat(int count, int length) {
+    while (count < length) {
+      System.out.print(" ");
+      count++;
+    }
+    count = 0;
+    return count;
+  }
+
   public static void main(String[] args) {
     border(30, 45, 5, 80);
   }
