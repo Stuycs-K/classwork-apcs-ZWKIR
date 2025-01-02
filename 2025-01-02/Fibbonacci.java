855bd55fcf52ed761d8153c2da7aa0e74a5ff6c6
Amy Shrestha <am226ys@gmail.com>
2025-01-02 11:35:45 -0500

    created Fibbonacci.java and tested fib(int n)

---
+++ 2025-01-02/Fibbonacci.java
@@ -0,0 +1,13 @@
+public class Fibbonacci {
+  public static int fib(int n) {
+    if (n < 2) {
+      return n;
+    } else {
+      return fib(n-1) + fib(n-2);
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(fib(7));
+  }
+}
