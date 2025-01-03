ae0205de4f70917599fc8894d18baee52953eb3c
Amy Shrestha <am226ys@gmail.com>
2025-01-03 18:37:34 -0500

    finished makeWords() to work for first test case

--- 2025-01-02/MakeWords.java
+++ 2025-01-02/MakeWords.java
@@ -4,8 +4,7 @@ public class MakeWords {
       System.out.println(result);
     } else {
       for (int i = 0; i < alphabet.length(); i++) {
-        result += alphabet.substring(0,1);
-        makeWords(remainingLetters - 1, result, alphabet);
+        makeWords(remainingLetters - 1, result + alphabet.substring(i, i+1), alphabet);
       }
     }
   }
