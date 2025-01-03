51ce20a831b39a653637e97d7461a5187eea7b7b
Amy Shrestha <am226ys@gmail.com>
2025-01-03 11:47:25 -0500

    working on makeWords

--- 2025-01-02/MakeWords.java
+++ 2025-01-02/MakeWords.java
@@ -1,9 +1,16 @@
 public class MakeWords {
   public static void makeWords(int remainingLetters, String result, String alphabet){
-    System.out.println(result);
+    if (remainingLetters < 1) {
+      System.out.println(result);
+    } else {
+      for (int i = 0; i < alphabet.length(); i++) {
+        result += alphabet.substring(0,1);
+        makeWords(remainingLetters - 1, result, alphabet);
+      }
+    }
   }
 
   public static void main(String[] args) {
-    System.out.println(makeWords(2,"","abc"));
+    makeWords(2,"","abc");
   }
 }
