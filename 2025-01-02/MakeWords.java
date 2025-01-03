e1d3b63bea552dc63513681df89d34a0a2ae1487
Amy Shrestha <am226ys@gmail.com>
2025-01-03 11:35:25 -0500

    started makeWords() function

---
+++ 2025-01-02/MakeWords.java
@@ -0,0 +1,9 @@
+public class MakeWords {
+  public static void makeWords(int remainingLetters, String result, String alphabet){
+    System.out.println(result);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(makeWords(2,"","abc"));
+  }
+}
